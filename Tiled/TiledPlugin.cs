efcaa92bc1a7cd0c80c864eaae32e64bc08c3d6e
thanatos-tshock <thanatos.tshock@gmail.com>
2021-07-14 20:07:17 +1000

    Update TiledPlugin.cs

--- Tiled/TiledPlugin.cs
+++ Tiled/TiledPlugin.cs
@@ -1,7 +1,7 @@
-using OTAPI.Tile;
+using ModFramework;
 using System;
+using System.Linq;
 using Terraria;
-using TerrariaApi.Server;
 using Tiled.OneDimension;
 using Tiled.Struct;
 using Tiled.TwoDimensions;
@@ -38,21 +38,10 @@ namespace Tiled
     /// 14 instructions to set a tile into the provider
     /// HeapTiles are stored in a flat one dimensional array, using one int32 offset + constants to calculate data offsets
     /// </remarks>
-	[ApiVersion(2, 1)]
-    public class TiledPlugin : TerrariaPlugin
+    public class Tiled
     {
-        public override string Author => "thanatos";
-        public override string Description => "Provides alternate memory usage and performance optimisations";
-        public override string Name => "Tiled";
-        public override Version Version => typeof(TiledPlugin).Assembly.GetName().Version;
-
-        public bool AcceptedWarning { get; set; }
-
-        public TiledPlugin(Main game) : base(game)
-        {
-        }
-
-        public override void Initialize()
+        [Modification(ModType.Runtime, "Loading Runtime Example!")]
+        public static void OnRunning()
         {
             string tileImplementation = null;
 
@@ -82,44 +71,7 @@ namespace Tiled
             Console.WriteLine($"Using tile provider: {Terraria.Main.tile.GetType().Name}");
         }
 
-        /*void AddCommands()
-        {
-            Commands.ChatCommands.Add(new Command((command_args) =>
-            {
-                if (command_args.Parameters.Count > 0)
-                {
-                    switch (command_args.Parameters[0])
-                    {
-                        case "set":
-                        case "hs":
-                        case "hotswap":
-                            if (command_args.Parameters.Count <= 1)
-                            {
-                                command_args.Player.SendInfoMessage($"Usage: tiled hotswap <provider>");
-                            }
-                            else
-                            {
-                                var hotprovider = ParseProviderName(command_args.Parameters[1]);
-                                if (hotprovider != null)
-                                {
-                                    command_args.Player.SendInfoMessage($"Changing to tile provider: {hotprovider.GetType().Name}");
-                                    SetProvider(hotprovider);
-                                    command_args.Player.SendInfoMessage($"Using tile provider: {Terraria.Main.tile.GetType().Name}");
-                                }
-                                else
-                                {
-                                    command_args.Player.SendInfoMessage($"No such provider named: {command_args.Parameters[1]}");
-                                }
-                            }
-                            return;
-                    }
-                }
-
-                command_args.Player.SendInfoMessage($"Using tile provider: {Terraria.Main.tile.GetType().Name}");
-            }, "tiled"));
-        }*/
-
-        public ITileCollection ParseProviderName(string name)
+        static ICollection<ITile> ParseProviderName(string name)
         {
             if (name == null)
                 return null;
@@ -129,7 +81,9 @@ namespace Tiled
             {
                 case "tsapi":
                 case "tshock":
-                    return new TileProvider();
+                    var type = Type.GetType("TerrariaApi.Server.TileProvider");
+                    if (type == null) throw new Exception("TShock is not installed, unable to use tsapi/tshock tile provider.");
+                    return (ICollection<ITile>)type.GetConstructors().Single().Invoke(null);
 
                 case "2d":
                     return new TwoDimensionTileProvider();
@@ -144,57 +98,13 @@ namespace Tiled
             return null;
         }
 
-        public bool SetProvider(ITileCollection provider)
+        static bool SetProvider(ICollection<ITile> provider)
         {
-            //if (!AcceptedWarning)
-            //{
-            //	Console.Write("This is an experimental command and is only intended for evaluation. Proceed? [Y/n]: ");
-
-            //	var accepted = Console.ReadKey().Key == ConsoleKey.Y;
-            //	if (accepted)
-            //	{
-
-            //	}
-
-            //	return false;
-            //}
-
-            ////todo transfer + unload if during game
-            //if (Netplay.IsListening)
-            //{
-            //    if (Terraria.Main.tile != null)
-            //    {
-            //        int x = 0;
-            //        int y = 0;
-            //        try
-            //        {
-            //            // trigger our internals to generate the data store
-            //            provider[0, 0]?.ClearTile();
-
-            //            x = 0;
-            //            for (x = 0; x < Terraria.Main.maxTilesX; x++)
-            //            {
-            //                y = 0;
-            //                for (y = 0; y < Terraria.Main.maxTilesY; y++)
-            //                {
-            //                    provider[x, y] = Terraria.Main.tile[x, y];
-            //                }
-            //            }
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            Console.WriteLine($"Error @{x}x{y}\n{ex}");
-            //            return false;
-            //        }
-            //    }
-            //}
-
             var previous = Terraria.Main.tile as IDisposable;
             Terraria.Main.tile = provider;
+
             if (previous != null)
-            {
                 previous.Dispose();
-            }
 
             GC.Collect();
 
